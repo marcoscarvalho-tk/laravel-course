7cdc58d10f020e12a958adaa533ff28b0cad80ac
marcoscarvalho-tk <marcoscarvalho.com@outlook.com>
2022-03-09 02:04:11 -0300

    inserindo parametros em Route

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,25 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
 
+/**Este tipo de Route, não conflita com a anterior
+ * devido estar recebendo parametros {nome, categoria, mensagem, etc}
+ * o próprio framework consegue diferenciá-la
+ * para tornar o parâmetro opcional adicione um ponto exclamação (?) após o parâmetro
+ * recomenda-se atribuir um valor padrão à variavel do parâmetro opcional.
+ * #######OBS: EXISTE UMA LIMITAÇÃO DE INTERPRETAÇÃO DO LARAVEL#####
+ * Os parâmetros opcionais só podem ser informados seguindo a ordem da dir. para esq.
+ * de modo que não haja parâmetro sem valor opcional
+ * este problema pode ser resolvido com condicionais
+ */
+Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', 
+      function(string $nome = 'Desconhecido', 
+      string $categoria = 'Categoria não informada', 
+      string $assunto = 'Sem assunto', 
+      string $mensagem = 'mensagem não informada'){
+    echo 'Estamos aqui:'.$nome.' '.$categoria.' '.$assunto.' '.$mensagem;
+    }
+);
+
 /* principais verbos http para controle de requisiçõe de servidores
 Route::get($uri, $callback)
 get
